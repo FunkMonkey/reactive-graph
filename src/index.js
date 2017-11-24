775ca3de3299b86a2b817fabb364561bb1dde679
AndSelm <and.selm+git@gmail.com>
2017-11-24 14:43:54 +0100

    FIX: internal renamings (mainly `operator` to `node`

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 /**
  * A user-defined operator configuration. Can basically be anything.
  *
- * @typedef {*}   OperatorConfig
+ * @typedef {*}   NodeConfig
  */
 
 /**
@@ -11,7 +11,7 @@
  *
  * @typedef {Object} NodeInfo
  * @property {string}          id
- * @property {OperatorConfig}  operatorConfig
+ * @property {NodeConfig}      config
  * @property {string[]}        sources - ID's of sources
 */
 
@@ -20,9 +20,9 @@
  * user-defined observable configuration (taken from a graph node's value) and
  * a set of ingoing `Observable`'s
  *
- * @callback insertOperatorCallback
+ * @callback insertNodeCallback
  * @param {string}            id
- * @param {OperatorConfig}    operatorConfig
+ * @param {NodeConfig}        config
  * @param {Observable[]}      sources
  * @return {Observable}
  */
@@ -41,7 +41,7 @@
  * operator using `Rx.Observable.let`.
  *
  * @callback getLetOperatorCallback
- * @param {OperatorConfig}    operatorConfig
+ * @param {NodeConfig}        config
  * @param {Observable[]}      sources         All sources, except for first one,
  *                                            which will be used as `source.let`
  * @return {LetObservableInfo}
@@ -64,7 +64,7 @@ function getTopsortedNodes( graph ) {
   const topsortedNodeIDs = graphlib.alg.topsort( graph );
 
   return topsortedNodeIDs.map( id => {
-    const operatorConfig = graph.node( id );
+    const config = graph.node( id );
     const inEdges = graph.inEdges( id );
 
     const sources = inEdges.map( inEdge => ( {
@@ -76,7 +76,7 @@ function getTopsortedNodes( graph ) {
 
     return {
       id,
-      operatorConfig,
+      config,
       sources
     };
   } );
@@ -84,35 +84,35 @@ function getTopsortedNodes( graph ) {
 
 /**
  * Connects the graph's nodes by iterating an array of topsorted node infos
- * and calling `insertOperator` for every node. Returns a dictionary of the
+ * and calling `insertNode` for every node. Returns a dictionary of the
  * created observables.
  *
  * @param  {NodeInfo[]}                      topsortedNodes
- * @param  {insertOperatorCallback}          insertOperator
+ * @param  {insertNodeCallback}              insertNode
  * @return {Object.<string, Observable>}
  */
-function connectOperators( topsortedNodes, insertOperator ) {
+function connectOperators( topsortedNodes, insertNode ) {
   return topsortedNodes.reduce( ( observables, node ) => {
     const sources = node.sources.map( preID => observables[ preID ] );
-    observables[ node.id ] = insertOperator( node.id, node.operatorConfig, sources );
+    observables[ node.id ] = insertNode( node.id, node.config, sources );
     return observables;
   }, {} );
 }
 
 /**
  * Runs a graph by connecting the graph's nodes in a topsorted fashion, calling
- * `insertOperator` for every node. Source nodes are sorted by the ingoing
+ * `insertNode` for every node. Source nodes are sorted by the ingoing
  * edge's value.
  *
  * This function is merely a shortcut for:
- * `connectOperators( getTopsortedNodes( graph ), insertOperator )`
+ * `connectOperators( getTopsortedNodes( graph ), insertNode )`
  *
  * @param  {graphlib.Graph}           graph
- * @param  {insertOperatorCallback}   insertOperator
+ * @param  {insertNodeCallback}       insertNode
  * @return {Object.<string, Observable>}
  */
-function run( graph, insertOperator ) {
-  return connectOperators( getTopsortedNodes( graph ), insertOperator );
+function run( graph, insertNode ) {
+  return connectOperators( getTopsortedNodes( graph ), insertNode );
 }
 
 
@@ -127,7 +127,7 @@ function run( graph, insertOperator ) {
  * the source for `let`, while the others are passed to `getOperatorContextAndArgs`.
  *
  * @param  {getLetOperatorCallback}  getOperatorContextAndArgs
- * @param  {OperatorConfig}          opConfig
+ * @param  {NodeConfig}              opConfig
  * @param  {Rx.Observable[] }        sources
  * @return {Rx.Observable}
  */
