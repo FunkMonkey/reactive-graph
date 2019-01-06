998057d13642c7c1a3f54f0900dcd157c11f0b37
AndSelm <and.selm+git@gmail.com>
2019-01-06 15:46:58 +0100

    MAINT: updating to gulp 4, plus updating devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,34 @@
-require( "./build-tool/tasks/build" );
+const gulp = require( 'gulp' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const babel = require( 'gulp-babel' );
+
+function watchTask( task ) {
+  const watcher = gulp.watch( [task.SRC_GLOB], task);
+  watcher.on( 'add', path => { console.log('File ' + path + ' was added, running tasks...'); });
+  watcher.on( 'change', path => { console.log('File ' + path + ' was changed, running tasks...'); });
+}
+
+// build task
+const SRC_GLOB =  './src/**/*.js';
+const DEST = './build';
+
+const build = () =>
+  gulp.src( SRC_GLOB, { cwd: __dirname } )
+      .pipe( sourcemaps.init() )
+      .pipe(
+        babel( {
+          presets: [ [ '@babel/preset-env', { 'targets': { 'node': '6.10' } } ] ]
+        } ) )
+      .pipe( sourcemaps.write( '.' ) )
+      .pipe( gulp.dest( DEST, { cwd: __dirname } ) );
+
+build.displayName = 'build';
+build.SRC_GLOB = SRC_GLOB;
+
+// watch:build task
+const watchBuild = () => watchTask( build );
+watchBuild.displayName = 'watch:build';
+
+// exports
+module.exports[build.displayName] = build;
+module.exports[watchBuild.displayName] = watchBuild;
